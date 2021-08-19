vymao/uppy

commit 3c8d7c404e6e17de77e718cfacc08c38e7e5a5d3

Dashboard: add `required` option to `metaFields` (#2896)
* Dashboard: add `required` option to `metaFields`

Fixes: https://github.com/transloadit/uppy/issues/2892

* use `<form>` element to allow users to use all built-in form features
from the web browser.

* remove `saveOnEnter` hack

The fields are now inside a form, saving on Enter is the default
behavior – and it doesn't short-circuit form validation checks.

* restrictions.requiredMetaFields

* fix documentation of restrictions.requiredMetaFields

* check for required meta fields on upload

* Use detached `<form>` element to avoid nesting `<form>` elements

* fixup! Use detached `<form>` element to avoid nesting `<form>` elements

* Use `cuid` to generate `<form>` `id`

packages/@uppy/dashboard/src/components/FileCard/index.js
@@ -1,5 +1,6 @@
 const { h, Component } = require('preact')
 const classNames = require('classnames')
+const cuid = require('cuid')
 const getFileTypeIcon = require('../../utils/getFileTypeIcon')
 const ignoreEvent = require('../../utils/ignoreEvent.js')
 const FilePreview = require('../FilePreview')
@@ -19,15 +20,8 @@ class FileCard extends Component {
     this.state = {
       formState: storedMetaData,
     }
-  }
 
-  saveOnEnter = (ev) => {
-    if (ev.keyCode === 13) {
-      ev.stopPropagation()
-      ev.preventDefault()
-      const file = this.props.files[this.props.fileCardFor]
-      this.props.saveFileCard(this.state.formState, file.id)
-    }
+    this.form.id = cuid()
   }
 
   updateMeta = (newVal, name) => {
@@ -39,13 +33,27 @@ class FileCard extends Component {
     })
   }
 
-  handleSave = () => {
+  form = document.createElement('form');
+
+  handleSave = (e) => {
+    e.preventDefault()
     const fileID = this.props.fileCardFor
     this.props.saveFileCard(this.state.formState, fileID)
   }
 
   handleCancel = () => {
     this.props.toggleFileCard(false)
+  }
+
+  // TODO(aduh95): move this to `UNSAFE_componentWillMount` when updating to Preact X+.
+  componentWillMount () {
+    this.form.addEventListener('submit', this.handleSave)
+    document.body.appendChild(this.form)
+  }
+
+  componentWillUnmount () {
+    this.form.removeEventListener('submit', this.handleSave)
+    document.body.removeChild(this.form)
   }
 
   renderMetaFields = () => {
@@ -56,6 +64,7 @@ class FileCard extends Component {
 
     return metaFields.map((field) => {
       const id = `uppy-Dashboard-FileCard-input-${field.id}`
+      const required = this.props.requiredMetaFields.includes(field.id)
       return (
         <fieldset key={field.id} className="uppy-Dashboard-FileCard-fieldset">
           <label className="uppy-Dashboard-FileCard-label" htmlFor={id}>{field.name}</label>
@@ -64,17 +73,18 @@ class FileCard extends Component {
               value: this.state.formState[field.id],
               onChange: (newVal) => this.updateMeta(newVal, field.id),
               fieldCSSClasses,
+              required,
+              form: this.form.id,
             }, h)
             : (
               <input
                 className={fieldCSSClasses.text}
                 id={id}
+                form={this.form.id}
                 type={field.type || 'text'}
+                required={required}
                 value={this.state.formState[field.id]}
                 placeholder={field.placeholder}
-                onKeyUp={this.saveOnEnter}
-                onKeyDown={this.saveOnEnter}
-                onKeyPress={this.saveOnEnter}
                 onInput={ev => this.updateMeta(ev.target.value, field.id)}
                 data-uppy-super-focusable
               />
@@ -112,6 +122,7 @@ class FileCard extends Component {
           <button
             className="uppy-DashboardContent-back"
             type="button"
+            form={this.form.id}
             title={this.props.i18n('finishEditingFile')}
             onClick={this.handleCancel}
           >
@@ -128,6 +139,7 @@ class FileCard extends Component {
                 type="button"
                 className="uppy-u-reset uppy-c-btn uppy-Dashboard-FileCard-edit"
                 onClick={() => this.props.openFileEditor(file)}
+                form={this.form.id}
               >
                 {this.props.i18n('editFile')}
               </button>
@@ -141,8 +153,8 @@ class FileCard extends Component {
           <div className="uppy-Dashboard-FileCard-actions">
             <button
               className="uppy-u-reset uppy-c-btn uppy-c-btn-primary uppy-Dashboard-FileCard-actionsBtn"
-              type="button"
-              onClick={this.handleSave}
+              type="submit"
+              form={this.form.id}
             >
               {this.props.i18n('saveChanges')}
             </button>
@@ -150,6 +162,7 @@ class FileCard extends Component {
               className="uppy-u-reset uppy-c-btn uppy-c-btn-link uppy-Dashboard-FileCard-actionsBtn"
               type="button"
               onClick={this.handleCancel}
+              form={this.form.id}
             >
               {this.props.i18n('cancel')}
             </button>

packages/@uppy/dashboard/types/index.d.ts
@@ -5,6 +5,8 @@ import DashboardLocale = require('./generatedLocale')
 type FieldRenderOptions = {
   value: string,
   onChange: (newVal: string) => void
+  fieldCSSClasses: { text: string }
+  required?: boolean
 }
 
 type PreactRender = (node: any, params: object | null, ...children: any[]) => any

examples/bundled/index.js
@@ -32,6 +32,7 @@ const uppy = new Uppy({
     showProgressDetails: true,
     proudlyDisplayPoweredByUppy: true,
     note: '2 files, images and video only',
+    restrictions: { requiredMetaFields: ['caption'] },
   })
   .use(GoogleDrive, { target: Dashboard, companionUrl: 'http://localhost:3020' })
   .use(Instagram, { target: Dashboard, companionUrl: 'http://localhost:3020' })

packages/@uppy/core/src/index.js
@@ -21,6 +21,21 @@ class RestrictionError extends Error {
     this.isRestriction = true
   }
 }
+if (typeof window.AggregateError === 'undefined') {
+  // eslint-disable-next-line no-global-assign
+  AggregateError = class AggregateError extends Error {
+    constructor (message, errors) {
+      super(message)
+      this.errors = errors
+    }
+  }
+}
+class AggregateRestrictionError extends AggregateError {
+  constructor (...args) {
+    super(...args)
+    this.isRestriction = true
+  }
+}
 
 /**
  * Uppy Core module.
@@ -51,6 +66,8 @@ class Uppy {
           0: 'You have to select at least %{smart_count} file',
           1: 'You have to select at least %{smart_count} files',
         },
+        missingRequiredMetaField: 'Missing required meta fields',
+        missingRequiredMetaFieldOnFile: 'Missing required meta fields in %{fileName}',
         // The default `exceedsSize2` string only combines the `exceedsSize` string (%{backwardsCompat}) with the size.
         // Locales can override `exceedsSize2` to specify a different word order. This is for backwards compat with
         // Uppy 1.9.x and below which did a naive concatenation of `exceedsSize2 + size` instead of using a locale-specific
@@ -108,6 +125,7 @@ class Uppy {
         maxNumberOfFiles: null,
         minNumberOfFiles: null,
         allowedFileTypes: null,
+        requiredMetaFields: [],
       },
       meta: {},
       onBeforeFileAdded: (currentFile) => currentFile,
@@ -539,6 +557,33 @@ class Uppy {
     const { minNumberOfFiles } = this.opts.restrictions
     if (Object.keys(files).length < minNumberOfFiles) {
       throw new RestrictionError(`${this.i18n('youHaveToAtLeastSelectX', { smart_count: minNumberOfFiles })}`)
+    }
+  }
+
+  /**
+   * Check if requiredMetaField restriction is met before uploading.
+   *
+   * @private
+   */
+  checkRequiredMetaFields (files) {
+    const { requiredMetaFields } = this.opts.restrictions
+    const { hasOwnProperty } = Object.prototype.hasOwnProperty
+
+    const errors = []
+    const fileIDs = Object.keys(files)
+    for (let i = 0; i < fileIDs.length; i++) {
+      const file = this.getFile(fileIDs[i])
+      for (let i = 0; i < requiredMetaFields.length; i++) {
+        if (!hasOwnProperty.call(file.meta, requiredMetaFields[i])) {
+          const err = new RestrictionError(`${this.i18n('missingRequiredMetaFieldOnFile', { fileName: file.name })}`)
+          errors.push(err)
+          this.showOrLogErrorAndThrow(err, { file, throwErr: false })
+        }
+      }
+    }
+
+    if (errors.length) {
+      throw new AggregateRestrictionError(`${this.i18n('missingRequiredMetaField')}`, errors)
     }
   }
 
@@ -1688,7 +1733,10 @@ class Uppy {
     }
 
     return Promise.resolve()
-      .then(() => this.checkMinNumberOfFiles(files))
+      .then(() => {
+        this.checkMinNumberOfFiles(files)
+        this.checkRequiredMetaFields(files)
+      })
       .catch((err) => {
         this.showOrLogErrorAndThrow(err)
       })

packages/@uppy/dashboard/src/index.js
@@ -1025,6 +1025,7 @@ module.exports = class Dashboard extends Plugin {
       parentElement: this.el,
       allowedFileTypes: this.uppy.opts.restrictions.allowedFileTypes,
       maxNumberOfFiles: this.uppy.opts.restrictions.maxNumberOfFiles,
+      requiredMetaFields: this.uppy.opts.restrictions.requiredMetaFields,
       showSelectedFiles: this.opts.showSelectedFiles,
       handleCancelRestore: this.handleCancelRestore,
       handleRequestThumbnail: this.handleRequestThumbnail,

examples/dev/Dashboard.js
@@ -58,6 +58,7 @@ module.exports = () => {
       username: 'John',
       license: 'Creative Commons',
     },
+    restrictions: { requiredMetaFields: ['caption'] },
   })
     .use(Dashboard, {
       trigger: '#pick-files',

website/src/examples/dashboard/app.es6
@@ -37,6 +37,7 @@ function uppyInit () {
 
   const uppy = new Uppy({
     logger: Uppy.debugLogger,
+    restrictions: { requiredMetaFields: ['caption'] }
   })
 
   uppy.use(Tus, { endpoint: 'https://tusd.tusdemo.net/files/', resume: true })

website/src/examples/dashboard/index.ejs
@@ -45,7 +45,8 @@ const uppy = new Uppy({
     maxFileSize: 1000000,
     maxNumberOfFiles: 3,
     minNumberOfFiles: 2,
-    allowedFileTypes: ['image/*', 'video/*']
+    allowedFileTypes: ['image/*', 'video/*'],
+    requiredMetaFields: ['caption'],
   }
 })
 .use(Dashboard, {

commit 53bc1209316695e0c8767fca00ea80e47f0e2a31

remove form attribute, as it is not supported in Preact 8
Bug introduced in #2896, fix in #3113 didn’t work for uppy 1.x, because Preact 8 ignores the form attribute. We’ve removed the use of form for 1.x, so you still get the required meta field functionality, but no feedback from the browser

Co-Authored-By: Antoine du Hamel <14309773+aduh95@users.noreply.github.com>

packages/@uppy/dashboard/src/components/FileCard/index.js
@@ -1,6 +1,5 @@
 const { h, Component } = require('preact')
 const classNames = require('classnames')
-const cuid = require('cuid')
 const getFileTypeIcon = require('../../utils/getFileTypeIcon')
 const ignoreEvent = require('../../utils/ignoreEvent.js')
 const FilePreview = require('../FilePreview')
@@ -20,8 +19,6 @@ class FileCard extends Component {
     this.state = {
       formState: storedMetaData,
     }
-
-    this.form.id = cuid()
   }
 
   updateMeta = (newVal, name) => {
@@ -33,8 +30,6 @@ class FileCard extends Component {
     })
   }
 
-  form = document.createElement('form');
-
   handleSave = (e) => {
     e.preventDefault()
     const fileID = this.props.fileCardFor
@@ -43,17 +38,6 @@ class FileCard extends Component {
 
   handleCancel = () => {
     this.props.toggleFileCard(false)
-  }
-
-  // TODO(aduh95): move this to `UNSAFE_componentWillMount` when updating to Preact X+.
-  componentWillMount () {
-    this.form.addEventListener('submit', this.handleSave)
-    document.body.appendChild(this.form)
-  }
-
-  componentWillUnmount () {
-    this.form.removeEventListener('submit', this.handleSave)
-    document.body.removeChild(this.form)
   }
 
   saveOnEnter = (ev) => {
@@ -83,22 +67,18 @@ class FileCard extends Component {
               onChange: (newVal) => this.updateMeta(newVal, field.id),
               fieldCSSClasses,
               required,
-              form: this.form.id,
             }, h)
             : (
               <input
                 className={fieldCSSClasses.text}
                 id={id}
-                form={this.form.id}
                 type={field.type || 'text'}
                 required={required}
                 value={this.state.formState[field.id]}
                 placeholder={field.placeholder}
-                // If `form` attribute is not supported, we need to capture pressing Enter to avoid bubbling in case Uppy is
-                // embedded inside a <form>.
-                onKeyUp={'form' in HTMLInputElement.prototype ? undefined : this.saveOnEnter}
-                onKeyDown={'form' in HTMLInputElement.prototype ? undefined : this.saveOnEnter}
-                onKeyPress={'form' in HTMLInputElement.prototype ? undefined : this.saveOnEnter}
+                onKeyUp={this.saveOnEnter}
+                onKeyDown={this.saveOnEnter}
+                onKeyPress={this.saveOnEnter}
                 onInput={ev => this.updateMeta(ev.target.value, field.id)}
                 data-uppy-super-focusable
               />
@@ -136,7 +116,6 @@ class FileCard extends Component {
           <button
             className="uppy-DashboardContent-back"
             type="button"
-            form={this.form.id}
             title={this.props.i18n('finishEditingFile')}
             onClick={this.handleCancel}
           >
@@ -153,7 +132,6 @@ class FileCard extends Component {
                 type="button"
                 className="uppy-u-reset uppy-c-btn uppy-Dashboard-FileCard-edit"
                 onClick={() => this.props.openFileEditor(file)}
-                form={this.form.id}
               >
                 {this.props.i18n('editFile')}
               </button>
@@ -167,11 +145,10 @@ class FileCard extends Component {
           <div className="uppy-Dashboard-FileCard-actions">
             <button
               className="uppy-u-reset uppy-c-btn uppy-c-btn-primary uppy-Dashboard-FileCard-actionsBtn"
-              // If `form` attribute is supported, we want a submit button to trigger the form validation.
-              // Otherwise, fallback to a classic button with a onClick event handler.
-              type={'form' in HTMLButtonElement.prototype ? 'submit' : 'button'}
-              onClick={'form' in HTMLButtonElement.prototype ? undefined : this.handleSave}
-              form={this.form.id}
+              // If `form` attribute is not in Preact 8, we can’t trigger the form validation.
+              // We use a classic button with a onClick event handler.
+              type="button"
+              onClick={this.handleSave}
             >
               {this.props.i18n('saveChanges')}
             </button>
@@ -179,7 +156,6 @@ class FileCard extends Component {
               className="uppy-u-reset uppy-c-btn uppy-c-btn-link uppy-Dashboard-FileCard-actionsBtn"
               type="button"
               onClick={this.handleCancel}
-              form={this.form.id}
             >
               {this.props.i18n('cancel')}
             </button>

packages/@uppy/dashboard/types/index.d.ts
@@ -7,7 +7,6 @@ type FieldRenderOptions = {
   onChange: (newVal: string) => void
   fieldCSSClasses: { text: string }
   required: boolean
-  form: string
 }
 
 type PreactRender = (node: any, params: object | null, ...children: any[]) => any